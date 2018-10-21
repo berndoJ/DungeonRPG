fc6dbe56234938b5c35b2fcca282ae4c11aabfcc
Johannes Berndorfer (berndoJ) <johannesberndorfer82@gmail.com>
2018-10-21 17:13:41 +0200

    Added feature 2d28-29f8-4268-2a80 - Main Menu

---
+++ DungeonRPG/Assets/Script/UI/MainMenuUI/MainMenuUIManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DungeonRPG.UI.MainMenu
+{
+    public class MainMenuUIManager : MonoBehaviour
+    {
+        #region Button Click Handlers
+
+        /// <summary>
+        /// Gets invoked when the button "New Game" got clicked.
+        /// </summary>
+        public void OnNewGameButtonClick()
+        {
+            SceneManager.LoadScene("Level0");
+        }
+
+        #endregion
+    }
+}
